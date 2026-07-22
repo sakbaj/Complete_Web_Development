ac877b8ddfe1e6bb3bdb7badbe02f161fdd41124
sakbaj <sakshambajpaihdi@gmail.com>
2026-07-22 09:58:30 +0000

    Jul 22, 2026, 3:28 PM

--- JavaScriptByPiyushGarg/10_Arrays/script.js
+++ JavaScriptByPiyushGarg/10_Arrays/script.js
@@ -27,6 +27,6 @@ const myArr = [0, 1, 2, 3, 4, 5, true, 'Saksham']
 // slice, splice
 
 console.log("A", myArr);
-const myn1 = myArr.slice(1, 3)
+const myn1 = myArr.slice(0, 3)
 console.log(myn1);
 console.log("B", myArr);
