2b7f066bc0b3e0e931786c5b7f75e98f6c644eba
sakbaj <sakshambajpaihdi@gmail.com>
2026-07-22 09:32:43 +0000

    Jul 22, 2026, 3:02 PM

--- JavaScriptByPiyushGarg/10_Arrays/script.js
+++ JavaScriptByPiyushGarg/10_Arrays/script.js
@@ -8,6 +8,8 @@ const myArr2 = new Array(1, 2, 3, 4)
 
 
 // Array methods
-myArr.push(6);
-myArr.push(7);
+// myArr.push(6);
+// myArr.push(7);
+// myArr.pop();
+myArr.unshift(10);
 console.log(myArr);
