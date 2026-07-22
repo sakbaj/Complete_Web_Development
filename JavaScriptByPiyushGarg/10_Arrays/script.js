c02393e5bb8ea70e21b8de09f35bca5b374a64a4
sakbaj <sakshambajpaihdi@gmail.com>
2026-07-22 09:26:49 +0000

    Jul 22, 2026, 2:56 PM

--- JavaScriptByPiyushGarg/10_Arrays/script.js
+++ JavaScriptByPiyushGarg/10_Arrays/script.js
@@ -1,5 +1,8 @@
 // Arrays In JavaScript
 
 const myArr = [0, 1, 2, 3, 4, 5, true, 'Saksham']
+const myHeros = ["Shaktiman", "Chota Bheem"]
+
+const myArr2 = new Array(1, 2, 3, 4)
 
 console.log(myArr[2]);
