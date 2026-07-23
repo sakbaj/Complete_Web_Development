47eb049c04699b7f9ca8786d383cb1410a5165c5
sakbaj <sakshambajpaihdi@gmail.com>
2026-07-23 09:25:32 +0000

    Jul 23, 2026, 2:55 PM

--- practiceWeb/PracticeJS/script.js
+++ practiceWeb/PracticeJS/script.js
@@ -20,13 +20,17 @@
 
 // Arrays 
 
-let a = ['HTML', 'CSS', 'Javascript', 'Java'];
-console.log("Original Array: "+a);
+// let a = ['HTML', 'CSS', 'Javascript', 'Java'];
+// console.log("Original Array: "+a);
 
 // a.pop(); // It removes the element of the last index of the array.
 
 // a.shift(); // It removes the element of the first index of the array.
 
-a.splice(2, 3); // It removes or replaces the element of the array.
+// a.splice(2, 3); // It removes or replaces the element of the array.
 
-console.log("Updated Array: "+a);
+// console.log("Updated Array: "+a);
+
+// Methods in Arrays Using JavaScript
+
+const 
