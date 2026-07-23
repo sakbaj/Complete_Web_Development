c58fd0054ee83d8f5d64a741a70a59c6dcbe86f8
sakbaj <sakshambajpaihdi@gmail.com>
2026-07-23 10:39:39 +0000

    Jul 23, 2026, 4:09 PM

--- JavaScriptByPiyushGarg/11_Arrays_02/script.js
+++ JavaScriptByPiyushGarg/11_Arrays_02/script.js
@@ -17,9 +17,16 @@ const allHeros = [...marvelHeros, ...dcHeros]; // Spread Operator
 const anotherArray = [1, 2, 3, [4, 5, 6], 7, [6, 7, [4, 5]]];
 
 const realAnotherArray = anotherArray.flat(Infinity);
-console.log(realAnotherArray);
+// console.log(realAnotherArray);
 
 
 console.log(Array.isArray("Saksham"));
 console.log(Array.from("Saksham"));
 console.log(Array.from({name: "Saksham"}));
+
+let score1 = 100;
+let score2 = 200;
+let score3 = 300;
+
+// console.log(Array.of(score1, score2, score3));
+console.log(Array.of(score1, score2, score3));
