4d2cc4cba8e73e492b0598366360214f1a5177dc
sakbaj <sakshambajpaihdi@gmail.com>
2026-07-23 10:10:15 +0000

    Jul 23, 2026, 3:40 PM

--- JavaScriptByPiyushGarg/10_Arrays/script.js
+++ JavaScriptByPiyushGarg/10_Arrays/script.js
@@ -18,13 +18,14 @@ const myArr2 = new Array(1, 2, 3, 4)
 // console.log(myArr.includes(9));
 // console.log(myArr.indexOf(3));
 
-const myArr = [0, 1, 2, 3, 4, 5, true, 'Saksham']
+
 // const newArr = myArr.join()
 
 // console.log(myArr);
 // console.log(typeof newArr);
 
 // slice, splice
+const myArr = [0, 1, 2, 3, 4, 5, true, 'Saksham']
 
 console.log("A", myArr);
 const myn1 = myArr.slice(1, 3)
