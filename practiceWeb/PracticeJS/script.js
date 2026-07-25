bc2752854744f4f6c9c05abaa31d987ab3bd15f3
sakbaj <sakshambajpaihdi@gmail.com>
2026-07-25 02:33:35 +0000

    Jul 25, 2026, 8:03 AM

--- practiceWeb/PracticeJS/script.js
+++ practiceWeb/PracticeJS/script.js
@@ -80,6 +80,6 @@
 const rollNo = [1, 2, 3, 4, 5, 6, 7, 8];
 
 // const res = rollNo.slice(2, 4);
-const res = rollNo.splice(2, 4);
+const res = rollNo.slice(2, 4);
 console.log(res);
 console.log(rollNo);
