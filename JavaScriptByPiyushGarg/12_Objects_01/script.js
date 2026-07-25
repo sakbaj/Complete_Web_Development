1c62fdd54d7a14e1d179b23713e6c3365b1a7c9f
sakbaj <sakshambajpaihdi@gmail.com>
2026-07-25 10:30:56 +0000

    Jul 25, 2026, 4:00 PM

--- JavaScriptByPiyushGarg/12_Objects_01/script.js
+++ JavaScriptByPiyushGarg/12_Objects_01/script.js
@@ -2,9 +2,12 @@
 // Object.create
 // object literals
 
+const mySym = Symbol("key1")
+
 const jsUser = {
     name: "Saksham",
     "full name": "Saksham Bajpai",
+    mySym: "mykey1",
     age: 18,
     location: "Lucknow",
     email: "saksham@gmail.com",
@@ -15,3 +18,4 @@ const jsUser = {
 // console.log(jsUser.email);
 // console.log(jsUser["email"]);
 console.log(jsUser["full name"]);
+console.log(jsUser.mySym);
