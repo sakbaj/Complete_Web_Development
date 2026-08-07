629c4eab03d49931f122c5d0569d9bc597a3f01d
sakbaj <sakshambajpaihdi@gmail.com>
2026-08-07 09:08:58 +0000

    Aug 7, 2026, 2:38 PM

--- JavaScriptByPiyushGarg/12_Objects_01/script.js
+++ JavaScriptByPiyushGarg/12_Objects_01/script.js
@@ -1,35 +1,39 @@
-// singleton
-// Object.create
+// // singleton
+// // Object.create
+// // object literals
+
+// const mySym = Symbol("key1")
+
+// const jsUser = {
+//     name: "Saksham",
+//     "full name": "Saksham Bajpai",
+//     mySym: "mykey1",
+//     age: 18,
+//     location: "Lucknow",
+//     email: "saksham@gmail.com",
+//     isLoggedIn: false,
+//     lastLoginDays: ["Monday", "Saturday"]
+// }
+
+// // console.log(jsUser.email);
+// // console.log(jsUser["email"]);
+// // console.log(jsUser["full name"]);
+// // console.log(jsUser[mySym]);
+
+// jsUser.email = "sakbaj@gmail.com";
+// // Object.freeze(jsUser);
+// jsUser.email = "dhc@gmail.com"
+// // console.log(jsUser);
+
+// jsUser.greeting = function () {
+//     console.log("Hello Js user");
+// }
+// jsUser.greeting2 = function () {
+//     console.log(`Hello Js user, ${this.name}`);
+// }
+// console.log(jsUser.greeting());
+// console.log(jsUser.greeting2());
+
+// Objects:  Singleton object
+
 // object literals
-
-const mySym = Symbol("key1")
-
-const jsUser = {
-    name: "Saksham",
-    "full name": "Saksham Bajpai",
-    mySym: "mykey1",
-    age: 18,
-    location: "Lucknow",
-    email: "saksham@gmail.com",
-    isLoggedIn: false,
-    lastLoginDays: ["Monday", "Saturday"]
-}
-
-// console.log(jsUser.email);
-// console.log(jsUser["email"]);
-// console.log(jsUser["full name"]);
-// console.log(jsUser[mySym]);
-
-jsUser.email = "sakbaj@gmail.com";
-// Object.freeze(jsUser);
-jsUser.email = "dhc@gmail.com"
-// console.log(jsUser);
-
-jsUser.greeting = function () {
-    console.log("Hello Js user");
-}
-jsUser.greeting2 = function () {
-    console.log(`Hello Js user, ${this.name}`);
-}
-console.log(jsUser.greeting());
-console.log(jsUser.greeting2());
