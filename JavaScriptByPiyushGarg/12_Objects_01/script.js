41e0a414d8a29b11b66a7f1cabab07e03a4ca49f
sakbaj <sakshambajpaihdi@gmail.com>
2026-08-07 09:16:13 +0000

    Aug 7, 2026, 2:46 PM

--- JavaScriptByPiyushGarg/12_Objects_01/script.js
+++ JavaScriptByPiyushGarg/12_Objects_01/script.js
@@ -37,3 +37,20 @@
 // Objects:  Singleton object
 
 // object literals
+
+const mySym = Symbol("key1")
+
+const JsUser = {
+    name: "Saksham",
+    "full name": "Saksham Bajpai",
+    mySym: "myKey1",
+    age: 20,
+    location: "Jaipur",
+    email: "saksh@gmail.com",
+    isLoggedIn: false,
+    lastLoginDays: ["Monday", "Saturday"]
+}
+
+console.log(JsUser["full name"]);
+console.log(JsUser["email"]);
+console.log(JsUser.mySym);
