91f44033c22b5ddb2e7c647f8a9f9f3926318881
sakbaj <sakshambajpaihdi@gmail.com>
2026-08-08 08:26:01 +0000

    Aug 8, 2026, 1:56 PM

--- practiceWeb/Practice/practice.js
+++ practiceWeb/Practice/practice.js
@@ -1,12 +1,16 @@
-// objects
+// // objects
 
-const obj = {
-    name: "Saksham",
-    course: "BCA",
-    email: "saksham@gmail.com",
-    rollNo: 226,
-    phone: 8797258105
-}
+// const obj = {
+//     name: "Saksham",
+//     course: "BCA",
+//     email: "saksham@gmail.com",
+//     rollNo: 226,
+//     phone: 8797258105
+// }
 
-console.log(obj.email);
-// console.log(obj[email]);
+// console.log(obj.email);
+// // console.log(obj[email]);
+
+let a = 34;
+console.log();
+
