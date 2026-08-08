7cfd04e441a217c03bab3cfd99f26d27d0e27ab0
sakbaj <sakshambajpaihdi@gmail.com>
2026-08-08 07:07:02 +0000

    Aug 8, 2026, 12:37 PM

--- practiceWeb/Practice/practice.js
+++ practiceWeb/Practice/practice.js
@@ -3,10 +3,10 @@
 const obj = {
     name: "Saksham",
     course: "BCA",
-    "email": "saksham@gmail.com",
+    email: "saksham@gmail.com",
     rollNo: 226,
     phone: 8797258105
 }
 
 console.log(obj.email);
-console.log(obj[email]);
+// console.log(obj[email]);
