954d0e4c3401b6b02530057a23cccc053f9d3f1d
sakbaj <sakshambajpaihdi@gmail.com>
2026-08-12 17:50:39 +0000

    Aug 12, 2026, 11:20 PM

--- practiceWeb/Practice/practice.js
+++ practiceWeb/Practice/practice.js
@@ -1,10 +1,18 @@
 // // objects
 
-const obj = {
-    name: "Saksham",
-    course: "BCA",
-    email: "saksh@gmail.com",
-    rollNo: 226
-}
+// First Way to define Objects in jS.
+// const obj = {
+//     name: "Saksham",
+//     course: "BCA",
+//     email: "saksh@gmail.com",
+//     rollNo: 226
+// }
 
-console.log(obj["email"]);
+// console.log(obj["email"]);
+
+// Second Way to define Objects in jS.
+
+let obj = new Object(); // Using Constructor method...
+obj.course = "BCA";
+
+console.log(obj.course);
