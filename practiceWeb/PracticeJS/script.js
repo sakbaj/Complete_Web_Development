16bd85bd4b9ffbc92c53da50572790f4869b7433
sakbaj <sakshambajpaihdi@gmail.com>
2026-08-14 17:06:19 +0000

    Aug 14, 2026, 10:36 PM

--- practiceWeb/PracticeJS/script.js
+++ practiceWeb/PracticeJS/script.js
@@ -65,7 +65,7 @@ const mySym = Symbol("key1");
 const stud = {
     name: "Saksham",
     "full name": "Saksham Bajpai",
-    mySym: "myKey1",
+    [mySym]: "myKey1",
     age: 20,
     location: "Jaipur",
     email: "saksh@gmail.com",
@@ -73,4 +73,4 @@ const stud = {
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(stud.mySym);
+console.log(stud[mySym]);
