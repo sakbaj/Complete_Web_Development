b4093826587edcf1ac028654f84b6d1b8ca8f810
sakbaj <sakshambajpaihdi@gmail.com>
2026-09-01 09:04:54 +0000

    Sep 1, 2026, 2:34 PM

--- practiceWeb/Practice/practice.js
+++ practiceWeb/Practice/practice.js
@@ -1,6 +1,13 @@
 // Arrays in javaScript
 
-let arr = [13, 34, 23, 53, 56, 51];
+// let arr = [13, 34, 23, 53, 56, 51];
 
-let new_arr = arr.splice(2, 4);
-console.log(new_arr);
+// let new_arr = arr.splice(2, 4);
+// console.log(new_arr);
+
+let numbers = [10, 20, 30, 40, 50];
+
+let removedItems = numbers.splice(1, 2);
+
+console.log(removedItems);
+console.log(numbers);
