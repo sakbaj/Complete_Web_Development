ebae8f8fbb39e51a51fb88125b6973d400043ea4
sakbaj <sakshambajpaihdi@gmail.com>
2026-09-01 18:01:52 +0000

    Sep 1, 2026, 11:31 PM

--- practiceWeb/Practice/practice.js
+++ practiceWeb/Practice/practice.js
@@ -1,9 +1,6 @@
 // Arrays in javaScript
 
-let arr = [13, 39, 23, 53, 56, 51];
+let arr = [4, 8, 16, 32];
 
-let new_arr = arr.splice(2, 4, 34, 45, 69);
-// slice(): In this, It doesn't change the original array...
-// splice(): In this, it modifies the original array...
-console.log(new_arr);
-console.log(arr);
+let doubled = arr.map((num) => num * 2);
+console.log(doubled);
