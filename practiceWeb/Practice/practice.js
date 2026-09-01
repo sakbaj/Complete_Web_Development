69a2241f2206ce9effcd2c85fcd7753e39e5c850
sakbaj <sakshambajpaihdi@gmail.com>
2026-09-01 17:46:22 +0000

    Sep 1, 2026, 11:16 PM

--- practiceWeb/Practice/practice.js
+++ practiceWeb/Practice/practice.js
@@ -2,7 +2,7 @@
 
 let arr = [13, 34, 23, 53, 56, 51];
 
-let new_arr = arr.slice(2, 4);
+let new_arr = arr.splice(2, 4, 34, 45, 69);
 // slice(): In this, It doesn't change the original array...
 // splice(): In this, it modifies the original array...
 console.log(new_arr);
